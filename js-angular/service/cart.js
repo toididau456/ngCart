09f67f28cde8d804ff8c68756898952c47a76d8b
Trung Kien <kienbt@tmh-techlab.vn>
2018-04-11 10:46:57 +0000

    11/3

--- js-angular/service/cart.js
+++ js-angular/service/cart.js
@@ -1,30 +1,44 @@
-app.run(['$rootScope', 'ngCart', function($rootScope, ngCart){
+app.run(['$rootScope', 'ngCart', function($rootScope, ngCart) {
     ngCart.init();
 }])
 .service('ngCart', [function() {
-    var total = 0;
-    this.init = function(){
+    this.init = function() {
         this.$cart = {
             items: []
         };
     };
-   
+
     this.addItem = function(id, name, price, img, quantity) {
         let items = this.$cart.items;
-        this.$cart.items.push({id, name, price, img, quantity});
+        this.$cart.items.push({ id, name, price, img, quantity });
     };
 
-    this.priceProduct = function(price, quantity){
-        return parseInt(price) * parseInt(quantity);
+    this.updateItem = function(id) {
+        var items = this.$cart.items;
+        items.forEach((value, key) => {
+            if (value.id == id) {
+                let quantity = angular.element(document.querySelector("#quantity_" + id)).val();
+                items[key].quantity = Math.abs(quantity);
+            }
+        });
+    };
+
+    this.deleteItem = function(id) {
+        var items = this.$cart.items;
+        this.$cart.items = items.filter(item => item.id != id);
     }
-    
+
+    this.priceProduct = function(price, quantity) {
+        return parseInt(price) * parseInt(quantity);
+    };
+
     this.totalPrice = function() {
         let items = this.ngCart.$cart.items;
-        var total = 0;
+        let total = 0;
         items.forEach(function(element, index) {
             total += (element.quantity * parseFloat(element.price));
         });
+
         return total;
     };
-
 }]);
